5cf95a48e790ef1fd487613ffd2668f726a84be8
Farhan Khan <kfarhan@vmware.com>
2021-07-13 21:52:59 +0530

    first commit

---
+++ 04. Basic mathematical operations/index.js
@@ -0,0 +1,19 @@
+// document.getElementById("count").innerText = 5
+
+let firstBatch = 7
+let secondBatch = 5
+let count = firstBatch % secondBatch
+console.log(count)
+
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+// let myAge = 35
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
---
+++ 19. Write your first string variable/index.js
@@ -0,0 +1,14 @@
+let username = "zia"
+
+// Create a variable, message, that stores the string: "You have tree new notifications"
+
+let message = "You have tree new notifications"
+
+console.log(username)
+
+// console.log(message + ", " + username + "!")
+
+let messageToUser = message + ", " + username + "!"
+console.log(messageToUser)
+
+
